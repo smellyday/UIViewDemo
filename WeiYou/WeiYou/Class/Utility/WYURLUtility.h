83f2e3ca3b08942268bb7a8f07303f4e70b08c04
owen <smellyday@gmail.com>
2014-07-30 14:31:26 +0800

    add keychain

---
+++ WeiYou/WeiYou/Class/Utility/WYURLUtility.h
@@ -0,0 +1,13 @@
+//
+//  WYURLUtility.h
+//  WeiYou
+//
+//  Created by zhangpan on 14-7-30.
+//  Copyright (c) 2014年 xiyuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYURLUtility : NSObject
+
+@end
---
+++ WeiYou/WeiYou/Class/Utility/WYURLUtility.m
@@ -0,0 +1,13 @@
+//
+//  WYURLUtility.m
+//  WeiYou
+//
+//  Created by zhangpan on 14-7-30.
+//  Copyright (c) 2014年 xiyuan. All rights reserved.
+//
+
+#import "WYURLUtility.h"
+
+@implementation WYURLUtility
+
+@end
